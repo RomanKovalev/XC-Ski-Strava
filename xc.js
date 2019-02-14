1ed41dcdf9351d6084747a9bb8c2b94b9b21725f
Roman Kovalev <3217548+RomanKovalev@users.noreply.github.com@users.noreply.github.com>
2019-02-14 12:44:11 +0300

    UI changes, reformat code

--- xc.js
+++ xc.js
@@ -6,9 +6,6 @@ card_wrapper.innerHTML = ski_tab + card_wrapper.innerHTML
 document.getElementById('ride-goals-tab').setAttribute('aria-selected', false)
 document.getElementById('ride-goals').setAttribute('aria-hidden', true)
 
-document.getElementsByClassName('js-snw-goals-upsell media upsell')[0].style.display = 'none'
-document.getElementsByClassName('js-snw-goals-upsell media upsell')[1].style.display = 'none'
-document.getElementsByClassName('js-snw-goals-upsell media upsell')[2].style.display = 'none'
 // document.getElementsByClassName('promo promo-fancy feed-entry card')[0].style.display='none';
 document.getElementsByClassName('promo promo-fancy feed-entry card')[0].remove()
 document.getElementsByClassName('upgrade')[0].style.display = 'none'
@@ -27,14 +24,23 @@ document.getElementsByClassName('tab-contents')[0].appendChild(ski_card_content)
 
 ski_card_content.getElementsByTagName('image')[0].setAttribute('href', chrome.extension.getURL('snowflake.png'))
 
+var banner = document.getElementsByClassName('js-snw-goals-upsell media upsell');
+banner[0].style.display = 'none'
+banner[1].style.display = 'none'
+banner[2].style.display = 'none'
+banner[3].getElementsByClassName('media-left')[0].remove();
+banner[3].getElementsByClassName('media-body pl-sm')[0].innerHTML = '<a style="cursor:pointer;">Setup you year XC ski goal</a>';
+banner[3].getElementsByClassName('media-body pl-sm')[0].style.textAlign='center';
+// banner[3].style.display = 'none'
+
+
 var url = 'https://www.strava.com/athlete/training_activities?activity_type=NordicSki'
 var pages
 var i
 var activities = []
-var later_activity_local = 0;
-var later_activity_remote = 0;
+var later_activity_local = 0
+var later_activity_remote = 0
 activities = JSON.parse(localStorage.getItem('nordicSki') || '[]')
-
 
 let results = fetch(
   url, {
@@ -50,19 +56,16 @@ let results = fetch(
     return res.json()
   })
   .then((data) => {
-  	if (activities.length != 0 ) {
+  	if (activities.length != 0) {
 	  	activities.forEach(function (element) {
-			if (element['id'] > later_activity_local) {later_activity_local = element['id']}
-		});
-		data['models'].forEach(function (element) {
-			if (element['id'] > later_activity_remote) {later_activity_remote = element['id']}
-		});
-		console.log(later_activity_local, later_activity_remote);
-  		if (later_activity_local == later_activity_remote) { return 1; };
-	}
-  	console.log('from here:', activities)
+        if (element['id'] > later_activity_local) { later_activity_local = element['id'] }
+      })
+      data['models'].forEach(function (element) {
+        if (element['id'] > later_activity_remote) { later_activity_remote = element['id'] }
+      })
+  		if (later_activity_local == later_activity_remote) { return 1 };
+    }
 
-	console.log('data', data);
     let promises = []
   			pages = Math.ceil(data['total'] / data['perPage'])
     for (i = pages; i > 0; i--) {
@@ -93,17 +96,16 @@ let results = fetch(
 			          	results.forEach(function (element) {
           result = result.concat(element)
         })
-        localStorage.setItem('nordicSki', JSON.stringify(result));
-        activities = JSON.parse(localStorage.getItem('nordicSki') || '[]');
-		return result
-		})
-			.catch((e) => {
-				console.log('alert')
-			})
+        localStorage.setItem('nordicSki', JSON.stringify(result))
+        activities = JSON.parse(localStorage.getItem('nordicSki') || '[]')
+        return result
+      })
+      .catch((e) => {
+        console.log('alert')
+      })
     return result
   })
   .catch(alert)
-
 
 var prevMonday = new Date()
 prevMonday.setDate(prevMonday.getDate() - (prevMonday.getDay() + 6) % 7)
@@ -123,13 +125,12 @@ activities.forEach(function (element) {
   }
 
   if (Date.parse(element['start_date']) >= Date.parse(prevMonday)) {
-    console.log("BINGO: ", element);
     common_week_counter = common_week_counter + element['distance_raw']
     elevation_gain = elevation_gain + element['elevation_gain_raw']
     elevation_unit = element['elevation_unit']
     moving_time = moving_time + element['moving_time_raw']
     switch (element['start_day']) {
-		  case 'Mon': days_week_counter['Mon'] = days_week_counter['Mon'] + element['distance_raw'] / 1000; console.log(element['distance_raw']);
+		  case 'Mon': days_week_counter['Mon'] = days_week_counter['Mon'] + element['distance_raw'] / 1000; 
 		  break
 		  case 'Tue': days_week_counter['Tue'] = days_week_counter['Tue'] + element['distance_raw'] / 1000
 		  break
@@ -146,7 +147,7 @@ activities.forEach(function (element) {
     }
   }
 })
-ski_card_content.getElementsByClassName('actual')[0].innerHTML = Math.round(common_week_counter / 10)/100;
+ski_card_content.getElementsByClassName('actual')[0].innerHTML = Math.round(common_week_counter / 10) / 100
 
 var max_day = Object.keys(days_week_counter).reduce(function (a, b) { return days_week_counter[a] > days_week_counter[b] ? a : b })
 
@@ -155,8 +156,6 @@ for (const [key, value] of Object.entries(days_week_counter)) {
 }
 var hours = ~~(moving_time / 3600)
 var minutes = Math.round((moving_time - (~~(moving_time / 3600) * 3600)) / 60)
-
-console.log(days_week_counter['Mon']);
 
 ski_card_content.getElementsByClassName('volume-bar-container')[0].getElementsByTagName('rect')[0].setAttribute('height', days_week_counter['Mon'] + 2)
 ski_card_content.getElementsByClassName('volume-bar-container')[0].getElementsByTagName('rect')[0].setAttribute('y', 46 - days_week_counter['Mon'])
@@ -184,7 +183,8 @@ ski_card_content.getElementsByClassName('list-stats')[0].childNodes[3].innerHTML
 var t = ski_card_content.getElementsByClassName('list-stats')[0].childNodes[1].getElementsByTagName('abbr')
 ski_card_content.getElementsByClassName('list-stats')[0].childNodes[1].innerHTML = hours + t[0].innerHTML + ' ' + minutes + t[1].innerHTML
 
-ski_card_content.getElementsByClassName('progress-container')[0].onclick = function (e) {
+
+ski_card_content.getElementsByClassName('media-body pl-sm')[0].onclick = function (e) {
   var modal = document.createElement('dialog')
   // get goal from storage
   modal.innerHTML = '<form><label>Set year goal<input type="number" min="0" step="1" value=0 style="margin:25px;"/></label><a class="btn btn-success btn-sm">Submit</a><a style="margin-left:15px;" class="btn btn-success btn-sm">Cancel</a></form>'
@@ -197,18 +197,18 @@ ski_card_content.getElementsByClassName('progress-container')[0].onclick = funct
     localStorage.setItem('nordicSkiYearGoal', ski_card_content.getElementsByTagName('input')[0].value)
     modal.close()
     let year_goal = localStorage.getItem('nordicSkiYearGoal') || '0'
-    ski_card_content.getElementsByClassName('card-body yearly-goal text-center')[0].getElementsByTagName('span')[0].innerHTML = Math.round(common_year_counter / 1000) + ' km / ' + year_goal + ' km';
-	location.reload();
+    ski_card_content.getElementsByClassName('card-body yearly-goal text-center')[0].getElementsByTagName('span')[0].innerHTML = Math.round(common_year_counter / 1000) + ' km / ' + year_goal + ' km'
+    location.reload()
   }
 }
 
 var year_goal = localStorage.getItem('nordicSkiYearGoal') || '0'
-ski_card_content.getElementsByClassName('card-body yearly-goal text-center')[0].getElementsByTagName('span')[0].innerHTML = Math.round(common_year_counter / 1000) + ' km / ' + year_goal + ' km';
+ski_card_content.getElementsByClassName('card-body yearly-goal text-center')[0].getElementsByTagName('span')[0].innerHTML = Math.round(common_year_counter / 1000) + ' km / ' + year_goal + ' km'
 
 var progress_year_bar_shadow = ski_card_content.getElementsByClassName('card-body yearly-goal text-center')[0].getElementsByTagName('rect')[0].cloneNode(true)
 
-progress_year_bar_shadow.setAttribute('class', 'progress-bar-container volume-bar highlighted');
-ski_card_content.getElementsByClassName('card-body yearly-goal text-center')[0].getElementsByTagName('g')[0].appendChild(progress_year_bar_shadow);
+progress_year_bar_shadow.setAttribute('class', 'progress-bar-container volume-bar highlighted')
+ski_card_content.getElementsByClassName('card-body yearly-goal text-center')[0].getElementsByTagName('g')[0].appendChild(progress_year_bar_shadow)
 
-if(year_goal == 0) { year_goal = 1};
-progress_year_bar_shadow.setAttribute('width', Math.round(common_year_counter * 248 / year_goal / 1000));
+if (year_goal == 0) { year_goal = 1 };
+progress_year_bar_shadow.setAttribute('width', Math.round(common_year_counter * 248 / year_goal / 1000))
