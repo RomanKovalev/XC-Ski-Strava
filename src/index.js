a9683eec91633d398dd67a3fa023d3f3c902f853
Roman Kovalev <3217548+RomanKovalev@users.noreply.github.com@users.noreply.github.com>
2019-08-08 22:38:53 +0300

    Added redux structure

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/app';
+import { Provider } from "react-redux";
+import store from './store'
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App /> // можно обернуть errorboundry - ОТлавливать ошибки в компонентах
+    </Provider>,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore } from "redux";
 import reducer from "./reducers";
 
-const store = createStorte(reducer)
+const store = createStore(reducer)
 
 export default store
