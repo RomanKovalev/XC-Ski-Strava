242478d5d1cb278939dc4ce19484375289f3d45f
Roman Kovalev <rk@intextech.ru>
2019-12-09 00:02:02 +0300

    Rewrite import algorithm

--- src/App.js
+++ src/App.js
@@ -11,12 +11,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    // this.db = useIndexedDB('activities');
   }
-  componentDidMount() {
-    initDB(DBConfig);
-  }
-  
+
   makerequest(url, page = 1) {
     const results = fetch(
       url + page, {
@@ -34,66 +30,44 @@ class App extends Component {
     return results
   }
 
-  addRecordsToDB(activities) {
-    const { add } = useIndexedDB('activities');
-    activities.forEach((activity, i, activities) => {
-      add(activity)
-        .then(event => { console.log('Inserted: ', event) },
-          error => { console.log('Exists already:', error) }
-        );
-    });
-  }
-
-  getLastSyncTime() {
-    const { getAll } = useIndexedDB('activities');
-    const data = getAll()
-      .then(all => {
-        console.log(all.length)
-        all.forEach((element, index, array) => {
-          if (element.sync_time) {
-            const sync_time = element.sync_time
-            delete array[index];
-            console.log('Sync time: ', sync_time)
-          } else {
-            console.log('Syn Time not exist: ', element.sync_time)
-          }
-        });
-        const lastActivityTime = all.reduce((prev, current) => prev.start_time > current.start_time ? prev : current, {});
-        console.log("lastActivityTime:", lastActivityTime)
-
-        all.forEach(element => {
-          if (element.start_time > lastActivityTime) {
-            console.log('Found new activity: ', element.start_time)
-          } else {
-            console.log('Activity is old')
+  handleClick() {
+    const lastSyncTime = JSON.parse(localStorage.getItem('lastSyncTime') || '[]')
+    const url = 'https://www.strava.com/athlete/training_activities?activity_type=NordicSki&page='
+    const results = this.makerequest(url)
+      .then(response => {
+        const skiActivities = JSON.parse(localStorage.getItem('skiActivities') || '[]')
+        const pages = Math.ceil(response.total / response.perPage)
+        response.models.forEach(element => {
+          if (element.start_time > lastSyncTime) {
+            skiActivities.push(element)
           }
         })
-      });
-  }
-
-  handleClick() {
-    const sync_time = this.getLastSyncTime()
-    console.log('SYNCTIME!!!!: ', sync_time)
-
-
-    const url = 'https://www.strava.com/athlete/training_activities?activity_type=NordicSki&page='
-    this.makerequest(url).then(response => {
-      this.addRecordsToDB(response.models)
-      const pages = Math.ceil(response.total / response.perPage)
-      for (let i = pages; i > 1; i--) {
-        this.makerequest(url, i).then(response => this.addRecordsToDB(response.models))
-      }
-    })
-    // this.addRecordsToDB([{ 'sync_time': new Date().toISOString(), workout_type: 'sync' }])
-    
-    // todo Resolve issue with creating 2 stores
-    // const { add:add_utils } = useIndexedDB('utils');
-    // const now = new Date().toISOString()
-    // add_utils({sync_time: now})
-    // .then(event => {console.log('Inserted: ', event)},
-    //   error => {console.log('Exists already:', error)}
-    // );
-
+        return { skiActivities, pages }
+      })
+      .then((data) => {
+        let promises = []
+        for (let page = 2; page <= data.pages; page++) {
+          promises.push(this.makerequest(url, page))
+        }
+        const resultPromise = Promise.all(promises)
+          .then((results) => {
+            let resultArray = []
+            const skiActivities = data.skiActivities
+            results.forEach(element => {
+              resultArray = resultArray.concat(element.models)
+            })
+            resultArray.forEach(element => {
+              if (element.start_time > lastSyncTime) {
+                data.skiActivities.push(element)
+              }
+            })
+            localStorage.setItem('skiActivities', JSON.stringify(skiActivities))
+          })
+      })
+      .then(() => {
+        localStorage.setItem('lastSyncTime', JSON.stringify(new Date().toISOString()))
+      })
+      .catch(error => console.log(error))
   }
 
   render() {
