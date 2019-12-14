b76bd6c90dfc4698534580d7f96ed9f3b6c2256e
Roman Kovalev <rk@intextech.ru>
2019-12-15 00:52:12 +0300

    Release!

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class App extends Component {
-
-  componentDidMount() {
-    console.log('MAIN APP DID MOUNTED')
-  }
   render() {
     return (
       <div className="App">
